0f76144469b9bca2ff9c446360e8005ab03f9374
Kumiko Haraguchi <haraguchi935@gmail.com>
2025-02-04 15:41:20 -0700

    Update comments

--- backend/app/db.py
+++ backend/app/db.py
@@ -3,13 +3,15 @@ from fastapi import HTTPException
 import psycopg2.pool
 from psycopg2 import DatabaseError
 
-# Read environment variables
+# Read environment variables for database configuration
 DB_USERNAME = os.getenv('DB_USERNAME', 'postgres')
 DB_PASSWORD = os.getenv('DB_PASSWORD', 'postgres')
 DB_HOST = os.getenv('DB_HOST', 'postgis')
 DB_PORT = os.getenv('DB_PORT', '5432')
 DB_NAME = os.getenv('DB_NAME', 'gis')
 dsn = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+# Initialize the connection pool
 pool = psycopg2.pool.SimpleConnectionPool(minconn=2, maxconn=4, dsn=dsn)
 
 # Dependency to get a database connection
