cf5fbe735734402dd57218c8b4018ee62c722551
Kumiko Haraguchi <haraguchi935@gmail.com>
2025-04-09 19:31:32 -0600

    (chore): add webfontDownload plugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,17 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import webfontDownload from "vite-plugin-webfont-dl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    webfontDownload([
+      "https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@300;400;600&display=swap",
+      "https://fonts.googleapis.com/css2?family=Bungee+Tint&:wght@400&display=swap",
+    ]),
+  ],
   server: {
     host: true, // Listen on all network interfaces
     port: 5173,
